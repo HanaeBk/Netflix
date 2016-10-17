0e8fe15593dedd4a174b752c9ca57360f6dc6674
ihs_2 <ihs_2@DESKTOP-6213J1J>
2016-10-17 08:07:52 +0200

    Change-Id: I0000000000000000000000000000000000000000

---
+++ Netflix/src/main/java/Model/Acteur.java
@@ -0,0 +1,49 @@
+package Model;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Acteur {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id ; 
+	private String nom;
+	private String prenom;
+	private List<Film> films;
+	
+	
+	// Getters & Setters
+	
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getPrenom() {
+		return prenom;
+	}
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	public List<Film> getFilms() {
+		return films;
+	}
+	public void setFilms(List<Film> films) {
+		this.films = films;
+	}
+	
+}
--- Netflix/src/main/java/Model/Category.java
+++ Netflix/src/main/java/Model/Category.java
@@ -1,5 +1,39 @@
 package Model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Category {
 
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
+	
+	
+	private String name;
+	
+	
+	// Getters & Setters
+	
+	
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	
+	
 }
